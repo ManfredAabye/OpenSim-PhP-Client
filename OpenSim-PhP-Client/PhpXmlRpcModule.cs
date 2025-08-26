464ab17e6ac7198e233e5515d6f1b9367264226c
ManfredAabye <zainhofer@gmx.de>
2025-08-26 13:55:37 +0200

    26082025

---
+++ OpenSim-PhP-Client/PhpXmlRpcModule.cs
@@ -0,0 +1,157 @@
+﻿using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using log4net;
+using MySql.Data.MySqlClient;
+using Nini.Config;
+using NSL.Certificate.Tools;
+using NSL.Network.XmlRpc;
+using Nwc.XmlRpc;
+using OpenMetaverse;
+using OpenSim.Data.MySQL.MySQLMoneyDataWrapper;
+using OpenSim.Framework;
+using OpenSim.Framework.Servers.HttpServer;
+using OpenSim.Modules.Currency;
+using OpenSim.Region.Framework.Scenes;
+using OpenSim.Services.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Transactions;
+using System.Xml;
+using static Mono.Security.X509.X520;
+using static OpenMetaverse.DllmapConfigHelper;
+using OpenSim.Server.Base;
+using OpenSim.Framework.Console;
+
+// todo list:
+
+// Wie in namespace OpenSim.Grid.MoneyServer, class MoneyXmlRpcModule die PHP Dateien abfangen und weiterverarbeiten.
+
+// Url to search service
+// SearchURL = "${Const|BaseURL}/search.php"
+
+// For V3 destination guide
+// DestinationGuide = "${Const|BaseURL}/guide.php"
+
+// For V3 avatar picker (( work in progress ))
+// AvatarPicker = "${Const|BaseURL}/avatars.php"
+
+// login page: optional: if it exists it will be used to tell the client to use
+// this as splash page
+// welcome = ${ Const | BaseURL}/welcome.php
+
+// web page of grid: optional: page providing further information about your grid
+// about = ${ Const | BaseURL}/about.php
+
+// account creation: optional: page providing further information about obtaining
+// a user account on your grid
+// register = ${ Const | BaseURL}/register.php
+
+// help: optional: page providing further assistance for users of your grid
+// help = ${ Const | BaseURL}/help.php
+
+// password help: optional: page providing password assistance for users of your grid
+// password = ${ Const | BaseURL}/password.php
+
+// a http page for grid status
+// GridStatus = ${ Const | BaseURL}/GridStatus.php
+// a RSS page for grid status
+// GridStatusRSS = ${ Const | BaseURL}/GridStatusRSS.php
+
+// optional web page for profiles
+// [AGENT_NAME] will be converted to Firstname.LastName by viewers
+// web_profile_url = http://webprofilesurl:ItsPort?name=[AGENT_NAME]
+
+// todo list end
+
+namespace OpenSim.PhP.Client
+{
+    internal class PhpXmlRpcModule
+    {
+        private BaseHttpServer m_httpServer;
+
+
+        public void Initialise()
+        {
+            RegisterStreamHandlers();
+        }
+
+        private void RegisterStreamHandlers()
+        {
+            // Registrierung für alle relevanten PHP-Endpunkte
+            m_httpServer.AddSimpleStreamHandler(new SimpleStreamHandler("/search.php", PHPProcesssearch));
+            m_httpServer.AddSimpleStreamHandler(new SimpleStreamHandler("/guide.php", PHPProcessguide));
+            m_httpServer.AddSimpleStreamHandler(new SimpleStreamHandler("/avatars.php", PHPProcessavatars));
+            m_httpServer.AddSimpleStreamHandler(new SimpleStreamHandler("/welcome.php", PHPProcesswelcome));
+            m_httpServer.AddSimpleStreamHandler(new SimpleStreamHandler("/about.php", PHPProcessabout));
+            m_httpServer.AddSimpleStreamHandler(new SimpleStreamHandler("/register.php", PHPProcessregister));
+            m_httpServer.AddSimpleStreamHandler(new SimpleStreamHandler("/help.php", PHPProcesshelp));
+            m_httpServer.AddSimpleStreamHandler(new SimpleStreamHandler("/password.php", PHPProcesspassword));
+            m_httpServer.AddSimpleStreamHandler(new SimpleStreamHandler("/GridStatus.php", PHPProcessGridStatus));
+            m_httpServer.AddSimpleStreamHandler(new SimpleStreamHandler("/GridStatusRSS.php", PHPProcessRSS));
+        }
+
+        public void PostInitialise()
+        {
+        }
+
+        public void RegisterHandlers()
+            {
+        }
+
+
+        private void PHPProcesssearch(IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            // todo: Implement search functionality
+        }
+        private void PHPProcessguide(IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            // todo: Implement search functionality
+        }
+        private void PHPProcessavatars(IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            // todo: Implement search functionality
+        }
+        private void PHPProcesswelcome(IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            // todo: Implement search functionality
+        }
+        private void PHPProcessabout(IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            // todo: Implement search functionality
+        }
+        private void PHPProcessregister(IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            // todo: Implement search functionality
+        }
+        private void PHPProcesshelp(IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            // todo: Implement search functionality
+        }
+        private void PHPProcesspassword(IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            // todo: Implement search functionality
+        }
+        private void PHPProcessGridStatus(IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            // todo: Implement search functionality
+        }
+        private void PHPProcessRSS(IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            // todo: Implement search functionality
+        }
+
+        public void UnregisterHandlers()
+            {
+        }
+    }
+}
---
+++ OpenSim-PhP-Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Mono.Addins;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenSim.PhP.Client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OpenSimulator Community")]
+[assembly: AssemblyProduct("OpenSim.PhP.Client")]
+[assembly: AssemblyCopyright("Copyright © OpenSimulator Community 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f554c84a-d8c7-41ea-833d-2483cde29e07")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
